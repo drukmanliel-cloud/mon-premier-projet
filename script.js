c46e37a5aa043195647c611328bb298851fc73e1
Vincent Zamora <drukmanliel@live.fr>
2026-08-17 17:53:58 +0200

    Ajout de la géolocalisation

--- script.js
+++ script.js
@@ -10,4 +10,30 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
 // Premier distributeur de test
 L.marker([48.8566, 2.3522])
     .addTo(map)
-    .bindPopup('🐾 Distributeur de sacs Toutou Map');
+       .bindPopup('🐾 Distributeur de sacs Toutou Map');
+
+// Géolocalisation de l'utilisateur
+const boutonLocalisation = document.querySelector('button');
+
+boutonLocalisation.addEventListener('click', () => {
+    if (!navigator.geolocation) {
+        alert("La géolocalisation n'est pas disponible sur votre appareil.");
+        return;
+    }
+
+    navigator.geolocation.getCurrentPosition(
+        (position) => {
+            const latitude = position.coords.latitude;
+            const longitude = position.coords.longitude;
+
+            map.setView([latitude, longitude], 16);
+
+            L.marker([latitude, longitude])
+                .addTo(map)
+                .bindPopup("📍 Vous êtes ici")
+                .openPopup();
+        },
+        () => {
+            alert("Impossible de récupérer votre position.");
+        }
+    );
